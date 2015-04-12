5240fbd4a8b3226ae22ad0bf514e47a2aae97cb5
aaronshan <shanruifeng@gmail.com>
2015-04-12 16:32:11 +0800

    add conf dir

---
+++ conf/sanfs-env.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# Set environment variables here.
+
+# The java implementation to use.  Java 1.6 required
+export JAVA_HOME=/export/java6/jdk1.6.0_45
+
+# Require setting SANFS_HOME
+export SANFS_HOME=/export/sanfs-0.1.0
+
+# Require setting SANFS_LOG_DIR
+export SANFS_LOG_DIR=$SANFS_HOME/logs
